03f396450adcb4a2d979f18c133adf3913867ab5
lishid <lishid@gmail.com>
2014-02-26 17:22:30 -0800

    Added Ivy, also track some time vars

--- src/com/linkedin/drelephant/hadoop/HadoopTaskData.java
+++ src/com/linkedin/drelephant/hadoop/HadoopTaskData.java
@@ -5,12 +5,24 @@ import org.apache.hadoop.mapred.TaskReport;
 
 public class HadoopTaskData {
     private HadoopCounterHolder counterHolder;
+    private long startTime;
+    private long endTime;
 
     public HadoopTaskData(TaskReport task) {
         counterHolder = new HadoopCounterHolder(task.getCounters());
+        startTime = task.getStartTime();
+        endTime = task.getFinishTime();
     }
 
     public HadoopCounterHolder getCounters() {
         return counterHolder;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
     }
 }
