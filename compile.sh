5d5eb1282ab9035c17d88af1e6008672a7e8b13b
Subbu Subramaniam <ssubrama@linkedin.com>
2014-11-24 16:53:27 -0800

    Fixed the build script.
    Changed the script so it prints out what it is doing.
    
    I suspect that somehow the directory name was not getting set correctly. Used a
    one-liner that seems to work on both Mac and Linux to determine directory of a script.
    
    Used braces to delimit the shell variable names. May be random variables were getting set?
    
    The zip command included a -x that pretty much amounted to nothing, since it excluded '*'.
    Changed the zip command to be more specific.
    
    Finally, added a trap command that fields ^C and exits the script.
    
    It seems to survive ctrl-C now.
    
    RB=399693
    BUGS=HADOOP-7816
    R=fli,akrai
    A=fli

--- compile.sh
+++ compile.sh
@@ -1,39 +1,43 @@
 #!/usr/bin/env bash
 
-script_dir=`which $0`
-script_dir=`dirname $script_dir`
-script_dir=`cd $script_dir; pwd`
+set -x
+trap "exit" SIGINT SIGTERM
 
-project_root=$script_dir
-start_script=$project_root/start.sh
-stop_script=$project_root/stop.sh
+project_root=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${project_root}
 
-cd $project_root
+start_script=${project_root}/start.sh
+stop_script=${project_root}/stop.sh
 
-rm -rf dist
+# Echo the value of pwd in the script so that it is clear what is being removed.
+rm -rf ${project_root}/dist
 mkdir dist
 
 play -Dhadoop.version=1 clean compile test dist
 
 cd target/universal
 
-unzip *.zip
+# TODO Good to add a check for exactly one zip here
+ZIP_NAME=`/bin/ls *.zip`
+unzip ${ZIP_NAME}
+DIST_NAME=${ZIP_NAME%.zip}
 
-DIST_NAME=$(find * -maxdepth 0 -type d -not -name "tmp")
+# Make two changes to bin/dr-elephant file
+# Add :$HADOOP_HOME/*:$HADOOP_HOME/lib/*:$HADOOP_HOME/conf to 'declare -r ...' line
+# Add a line addJava "-Djava.library.path=$HADOOP_HOME/lib/native/Linux-amd64-64" right after
+sed -i.bak $'/declare -r app_classpath/s/.$/:$HADOOP_HOME\/*:$HADOOP_HOME\/lib\/*:$HADOOP_HOME\/conf"\\\naddJava "-Djava.library.path=$HADOOP_HOME\/lib\/native\/Linux-amd64-64"\\\n/'  ${DIST_NAME}/bin/dr-elephant
 
-sed -i.bak $'/declare -r app_classpath/s/.$/:$HADOOP_HOME\/*:$HADOOP_HOME\/lib\/*:$HADOOP_HOME\/conf"\\\naddJava "-Djava.library.path=$HADOOP_HOME\/lib\/native\/Linux-amd64-64"\\\n/'  $DIST_NAME/bin/dr-elephant
+chmod +x ${DIST_NAME}/bin/dr-elephant
 
-chmod +x $DIST_NAME/bin/dr-elephant
+cp $start_script ${DIST_NAME}/bin/
 
-cp $start_script $DIST_NAME/bin/
+cp $stop_script ${DIST_NAME}/bin/
 
-cp $stop_script $DIST_NAME/bin/
+zip -r ${DIST_NAME}-h1.zip ${DIST_NAME} -x "${DIST_NAME}/*.zip"
 
-zip -r $DIST_NAME-h1.zip $DIST_NAME -x $DIST_NAME/*.zip *.zip tmp/\*  *
+mv ${DIST_NAME}-h1.zip ${project_root}/dist/
 
-mv $DIST_NAME-h1.zip $project_root/dist/
-
-cd $project_root
+cd ${project_root}
 
 play -Dhadoop.version=2 clean compile test dist
 
@@ -41,14 +45,14 @@ cd target/universal
 
 unzip *.zip
 
-sed -i.bak $'/declare -r app_classpath/s/.$/:$HADOOP_HOME\/share\/hadoop\/common\/*:$HADOOP_HOME\/share\/hadoop\/common\/lib\/*:$HADOOP_HOME\/share\/hadoop\/hdfs\/*:$HADOOP_CONF_DIR"\\\naddJava "-Djava.library.path=$HADOOP_HOME\/lib\/native"\\\n/'  $DIST_NAME/bin/dr-elephant
+sed -i.bak $'/declare -r app_classpath/s/.$/:$HADOOP_HOME\/share\/hadoop\/common\/*:$HADOOP_HOME\/share\/hadoop\/common\/lib\/*:$HADOOP_HOME\/share\/hadoop\/hdfs\/*:$HADOOP_CONF_DIR"\\\naddJava "-Djava.library.path=$HADOOP_HOME\/lib\/native"\\\n/'  ${DIST_NAME}/bin/dr-elephant
 
-chmod +x $DIST_NAME/bin/dr-elephant
+chmod +x ${DIST_NAME}/bin/dr-elephant
 
-cp $start_script $DIST_NAME/bin
+cp $start_script ${DIST_NAME}/bin
 
-cp $stop_script $DIST_NAME/bin
+cp $stop_script ${DIST_NAME}/bin
 
-zip -r $DIST_NAME-h2.zip $DIST_NAME -x $DIST_NAME/*.zip *.zip tmp/\*  *
+zip -r ${DIST_NAME}-h2.zip ${DIST_NAME} -x "${DIST_NAME}/*.zip"
 
-mv $DIST_NAME-h2.zip $project_root/dist/
+mv ${DIST_NAME}-h2.zip $project_root/dist/
